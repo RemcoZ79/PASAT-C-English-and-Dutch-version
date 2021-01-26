1e3010fc43951a604f20d7b0fd9c09aa375845d8
RemcoZ79 <77976280+RemcoZ79@users.noreply.github.com>
2021-01-26 14:58:58 +0100

    Update PASAT_EN.py

--- PASAT_EN.py
+++ PASAT_EN.py
@@ -1,15 +1,5 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This experiment was created using PsychoPy3 Experiment Builder (v2020.2.4),
-    on januari 26, 2021, at 14:50
-If you publish work using this script the most relevant publication is:
-
-    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
-        PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
-        https://doi.org/10.3758/s13428-018-01193-y
-
-"""
 
 from __future__ import absolute_import, division
 
